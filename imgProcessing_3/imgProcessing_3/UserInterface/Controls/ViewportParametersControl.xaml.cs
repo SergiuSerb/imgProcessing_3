3a6d52387c3da9fde81c10af8de1c265ad8e0a24
Sergiu Serb <ovidiusergiu_7@yahoo.com>
2021-11-20 17:04:08 +0200

    Created Viewport control. Added basic logic.

---
+++ imgProcessing_3/imgProcessing_3/UserInterface/Controls/ViewportParametersControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace imgProcessing_3.UserInterface.Controls
+{
+    /// <summary>
+    ///     Interaction logic for ViewportParametersControl.xaml
+    /// </summary>
+    public partial class ViewportParametersControl : UserControl
+    {
+        public delegate void ViewportParametersChangedHandler(object sender, double rotation, string renderMode);
+
+        private static readonly IList<string> RenderPresets = new List<string> { "3D", "2D" };
+
+        private string currentlySelectedRenderMode;
+        private double currentRotation;
+
+        public ViewportParametersControl()
+        {
+            InitializeComponent();
+            InitializePresetDropdown();
+        }
+
+        private void InitializePresetDropdown()
+        {
+            foreach (string item in RenderPresets)
+            {
+                presetDropdown.Items.Add(item);
+            }
+        }
+
+        public event ViewportParametersChangedHandler ViewportParametersChanged;
+
+        private void presetDropdown_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            currentlySelectedRenderMode = presetDropdown.Text;
+            ViewportParametersChanged?.Invoke(this, currentRotation, currentlySelectedRenderMode);
+        }
+
+        private void rotationSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            currentRotation = rotationSlider.Value;
+            ViewportParametersChanged?.Invoke(this, currentRotation, currentlySelectedRenderMode);
+        }
+    }
+}

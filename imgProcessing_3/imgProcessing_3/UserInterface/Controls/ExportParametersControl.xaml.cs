97e99f612dc037cac3b5c6219e1b895ef8d53172
Sergiu Serb <ovidiusergiu_7@yahoo.com>
2021-11-22 00:26:41 +0200

    Created ExportParametersControl.

---
+++ imgProcessing_3/imgProcessing_3/UserInterface/Controls/ExportParametersControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace imgProcessing_3.UserInterface.Controls
+{
+    /// <summary>
+    ///     Interaction logic for ExportParametersControl.xaml
+    /// </summary>
+    public partial class ExportParametersControl : UserControl
+    {
+        public int CurrentSize { get; set; }
+
+        public ExportParametersControl()
+        {
+            CurrentSize = 2;
+            InitializeComponent();
+        }
+
+
+        private void sizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            CurrentSize = (int)Math.Pow(2, sizeSlider.Value);
+            UpdateSizeTextBox();
+        }
+
+        private void UpdateSizeTextBox()
+        {
+            if (sizeValue != null)
+            {
+                sizeValue.Text = $"{CurrentSize} x {CurrentSize}";
+            }
+        }
+
+        private void pathTextbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+    }
+}

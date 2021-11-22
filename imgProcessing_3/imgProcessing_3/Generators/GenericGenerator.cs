11c80204f17ca0f404704673290607c1fa59dca3
Sergiu Serb <ovidiusergiu_7@yahoo.com>
2021-11-22 22:19:45 +0200

    Finished individual face generation functionality.

--- imgProcessing_3/imgProcessing_3/Generators/GenericGenerator.cs
+++ imgProcessing_3/imgProcessing_3/Generators/GenericGenerator.cs
@@ -53,6 +53,26 @@ namespace imgProcessing_3.Generators
             {
                 GenerateFace1();
             }
+
+            if (faces[2])
+            {
+                GenerateFace2();
+            }
+
+            if (faces[3])
+            {
+                GenerateFace3();
+            }
+
+            if (faces[4])
+            {
+                GenerateFace4();
+            }
+
+            if (faces[5])
+            {
+                GenerateFace5();
+            }
         }
 
         private void GenerateFace0()
@@ -67,7 +87,7 @@ namespace imgProcessing_3.Generators
                 }
             }
 
-            bitmap.Save(path + "face0.bmp", ImageFormat.Bmp);
+            bitmap.Save(path + "face0.jpeg", ImageFormat.Jpeg);
         }
 
         private void GenerateFace1()
@@ -82,14 +102,74 @@ namespace imgProcessing_3.Generators
                 }
             }
 
-            bitmap.Save(path + "face1.bmp", ImageFormat.Bmp);
+            bitmap.Save(path + "face1.jpeg", ImageFormat.Jpeg);
+        }
+
+        private void GenerateFace2()
+        {
+            Bitmap bitmap = new Bitmap(size, size);
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    bitmap.SetPixel(i, j, GetColourForCoord(i, j, 0));
+                }
+            }
+
+            bitmap.Save(path + "face2.jpeg", ImageFormat.Jpeg);
+        }
+
+        private void GenerateFace3()
+        {
+            Bitmap bitmap = new Bitmap(size, size);
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    bitmap.SetPixel(i, j, GetColourForCoord(i, size - j, size));
+                }
+            }
+
+            bitmap.Save(path + "face3.jpeg", ImageFormat.Jpeg);
+        }
+
+        private void GenerateFace4()
+        {
+            Bitmap bitmap = new Bitmap(size, size);
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    bitmap.SetPixel(i, j, GetColourForCoord(0, i, j));
+                }
+            }
+
+            bitmap.Save(path + "face4.jpeg", ImageFormat.Jpeg);
+        }
+
+        private void GenerateFace5()
+        {
+            Bitmap bitmap = new Bitmap(size, size);
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    bitmap.SetPixel(i, j, GetColourForCoord(size, size - i, j));
+                }
+            }
+
+            bitmap.Save(path + "face5.jpeg", ImageFormat.Jpeg);
         }
 
         private Color GetColourForCoord(int x, int y, int z)
         {
-            int red = (int)(x * (255.0 / size));
-            int green = (int)(y * (255.0 / size));
-            int blue = (int)(z * (255.0 / size));
+            int red = (int)(x * (255.0 / size) * strengthR);
+            int green = (int)(y * (255.0 / size) * strengthG);
+            int blue = (int)(z * (255.0 / size) * strengthB);
 
             return Color.FromArgb(red, green, blue);
         }
